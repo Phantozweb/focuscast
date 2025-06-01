9b27baf31bb8255dd745759dbcc6ad9f8a937250
Phantoz <tellersecret894@gmail.com>
2025-06-01 16:05:08 +0000

    <meta name="google-site-verification" content="RYhFog1Yp3g2Il7DThaudIMk7PJH9NIwwilaA2BzC2s" /> add this

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="google-site-verification" content="RYhFog1Yp3g2Il7DThaudIMk7PJH9NIwwilaA2BzC2s" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
