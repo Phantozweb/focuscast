94436899c42bc6838907519b9d7f48568729e8ac
Phantoz <tellersecret894@gmail.com>
2025-10-16 11:36:09 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -30,7 +30,7 @@ const nextConfig: NextConfig = {
         pathname: '/**',
       },
       {
-        protocol: 'https,"',
+        protocol: 'https',
         hostname: 'i.iili.io',
         port: '',
         pathname: '/**',
